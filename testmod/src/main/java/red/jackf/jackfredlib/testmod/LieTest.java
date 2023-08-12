7fd3958b66b6eed77bc9338db70b92ae39669e6d
Jack Pearce <jack@jackf.red>
2023-08-12 02:28:29 +0100

    [feat] add util class for glowing cubes

--- testmod/src/main/java/red/jackf/jackfredlib/testmod/LieTest.java
+++ testmod/src/main/java/red/jackf/jackfredlib/testmod/LieTest.java
@@ -2,56 +2,25 @@ package red.jackf.jackfredlib.testmod;
 
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.SharedConstants;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.phys.Vec3;
 import red.jackf.jackfredlib.api.lying.Debris;
 import red.jackf.jackfredlib.api.lying.Lies;
 import red.jackf.jackfredlib.api.lying.entity.EntityLie;
+import red.jackf.jackfredlib.api.lying.entity.EntityUtil;
 
 public class LieTest {
-    private static Slime makeSlime(ServerLevel serverLevel, BlockPos pos) {
-        var entity = EntityType.SLIME.create(
-                serverLevel,
-                null,
-                null,
-                pos.above(),
-                MobSpawnType.COMMAND,
-                false,
-                false
-        );
-        if (entity != null) {
-            entity.setSize(1, false);
-            entity.setXRot(0f);
-            entity.setYRot(0f);
-            entity.setOldPosAndRot();
-            entity.setYHeadRot(entity.getYRot());
-            entity.setYBodyRot(entity.getYRot());
-            entity.setNoGravity(true);
-            entity.setNoAi(true);
-            var x = pos.getX() + 0.5;
-            var y = pos.getY() + 0.5 - entity.getBbHeight() / 2;
-            var z = pos.getZ() + 0.5;
-            entity.moveTo(x, y, z);
-            entity.setGlowingTag(true);
-            entity.setInvisible(true);
-        }
-        return entity;
-    }
-
     public static void setup() {
         UseBlockCallback.EVENT.register((player, level, hand, hitResult) -> {
             if (level instanceof ServerLevel serverLevel && player instanceof ServerPlayer serverPlayer) {
                 var handStack = player.getItemInHand(hand);
                 if (handStack.is(Items.DIAMOND_AXE)) {
-                    var lie = Lies.INSTANCE.addEntity(serverPlayer, EntityLie.builder(makeSlime(serverLevel, hitResult.getBlockPos().offset(hitResult.getDirection()
+                    var lie = Lies.INSTANCE.addEntity(serverPlayer, EntityLie.builder(EntityUtil.glowingCube(serverLevel, hitResult.getBlockPos().offset(hitResult.getDirection()
                             .getNormal())))
                             .onLeftClick((activeLie, shiftDown, pos) -> {
                                 activeLie.player().sendSystemMessage(Component.literal("left clicked " + pos));
@@ -61,7 +30,7 @@ public class LieTest {
                                     activeLie.player().sendSystemMessage(Component.literal("right clicked" + pos));
                             })).onTick(activeLie -> {
                                 var entity = activeLie.lie().entity();
-                                entity.setPos(entity.position().add(0, 0.025, 0));
+                                entity.setPos(entity.position().add(new Vec3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).scale(0.1)));
                             })
                             .build());
                     Debris.INSTANCE.schedule(lie, 20 * SharedConstants.TICKS_PER_SECOND);
