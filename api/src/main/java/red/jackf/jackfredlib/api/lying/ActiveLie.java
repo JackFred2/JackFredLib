b399da9409c99fda5cc0dd3054ea476226aa9af9
Jack Pearce <jack@jackf.red>
2023-08-14 23:00:51 +0100

    [feat] prevent double calling of fade callbacks

--- api/src/main/java/red/jackf/jackfredlib/api/lying/ActiveLie.java
+++ api/src/main/java/red/jackf/jackfredlib/api/lying/ActiveLie.java
@@ -1,19 +1,61 @@
 package red.jackf.jackfredlib.api.lying;
 
 import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.ApiStatus;
 import red.jackf.jackfredlib.impl.lying.LiesImpl;
 
 /**
  * Represents an active lie being shown to a given player.
  */
-public record ActiveLie<L extends Lie<L>>(ServerPlayer player, L lie) {
+public final class ActiveLie<L extends Lie<L>> {
+    private final ServerPlayer player;
+    private final L lie;
+    private boolean faded = false;
+
+    /**
+     * Create a new active lie instance. Should not be used directly; use a method in {@link Lies}
+     *
+     * @param player Player to send the lie to
+     * @param lie Lie to send to said player
+     */
+    @ApiStatus.Internal
+    public ActiveLie(ServerPlayer player, L lie) {
+        this.player = player;
+        this.lie = lie;
+    }
 
     /**
      * Fade this lie. Removes this lie from tracking, and from the given player's client.
      */
     public void fade() {
+        if (this.faded) return;
         LiesImpl.LOGGER.debug("Fading lie for {}", player.getName());
         this.lie.fade(this);
         LiesImpl.INSTANCE.remove(this);
+        this.faded = true;
+    }
+
+    /**
+     * Whether this active lie instance has already faded; i.e. no longer displayed to the player.
+     * @return If this active lie is faded.
+     */
+    public boolean hasFaded() {
+        return this.faded;
+    }
+
+    /**
+     * The player that this active lie is for (being displayed to).
+     * @return Player this active lie instance is for
+     */
+    public ServerPlayer player() {
+        return player;
+    }
+
+    /**
+     * The lie that is being displayed to the player. May be shared with a different active lie instance.
+     * @return Lie being displayed to the given player
+     */
+    public L lie() {
+        return lie;
     }
 }
