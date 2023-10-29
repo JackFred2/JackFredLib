3f1f13bbf0d9609d8b26ee37077e55f88b94ac73
Jack Pearce <jack@jackf.red>
2023-10-29 03:03:12 +0000

    [fix] lie interactions not working

--- jackfredlib-lying/src/main/java/red/jackf/jackfredlib/mixins/lying/entity/ServerboundInteractPacketAccessor.java
+++ jackfredlib-lying/src/main/java/red/jackf/jackfredlib/mixins/lying/entity/ServerboundInteractPacketAccessor.java
@@ -1,17 +1,14 @@
 package red.jackf.jackfredlib.mixins.lying.entity;
 
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 /**
  * Get the entity ID of an entity to check if it's an entity lie.
  */
 @Mixin(ServerboundInteractPacket.class)
 public interface ServerboundInteractPacketAccessor {
-    @Invoker("getTarget")
-    @Nullable Entity invokeGetTarget(ServerLevel level);
+    @Accessor("entityId")
+    int invokeGetEntityId();
 }
