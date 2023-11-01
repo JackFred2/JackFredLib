6643d3b9209725c2d40294063083217b6e607754
Jack Pearce <jack@jackf.red>
2023-11-01 16:35:09 +0000

    [feat] updatable grammar

--- jackfredlib-config/src/main/java/red/jackf/jackfredlib/api/config/ConfigHandler.java
+++ jackfredlib-config/src/main/java/red/jackf/jackfredlib/api/config/ConfigHandler.java
@@ -1,15 +1,18 @@
 package red.jackf.jackfredlib.api.config;
 
+import blue.endless.jankson.JsonGrammar;
 import org.jetbrains.annotations.NotNull;
 import red.jackf.jackfredlib.impl.config.ConfigHandlerBuilderImpl;
 
 /**
  * A ConfigHandler is your interface point to access your config. Create a new instance using {@link #builder(Class)}.
+ *
  * @param <T> Type of your config file.
  */
 public interface ConfigHandler<T extends Config<T>> {
     /**
      * Get a default instance of the config. A new instance is generated each call.
+     *
      * @return A new default instance of your config.
      */
     T getDefault();
@@ -17,6 +20,7 @@ public interface ConfigHandler<T extends Config<T>> {
     /**
      * <p>Get the current instance of your config. If this is the first time calling, load it from a file.</p>
      * <p>If an error occurs during loading of the file, then a temporary default instance will be used.
+     *
      * @return Current instance of the config.
      */
     T instance();
@@ -33,10 +37,19 @@ public interface ConfigHandler<T extends Config<T>> {
     void save();
 
     /**
+     * Update the Jankson grammar for this handler.
+     *
+     * @apiNote Not recommended to update to a new grammar with a different <code>allowBareRootObject</code> setting.
+     * @param newGrammar New Jankson grammar to use.
+     */
+    void changeGrammar(@NotNull JsonGrammar newGrammar);
+
+    /**
      * Create a new builder for a config handler.
+     *
      * @param configClass Class of your config file.
+     * @param <T>         Class of your config file.
      * @return A new config handler builder.
-     * @param <T> Class of your config file.
      */
     static <T extends Config<T>> ConfigHandlerBuilder<T> builder(@NotNull Class<T> configClass) {
         return new ConfigHandlerBuilderImpl<>(configClass);
