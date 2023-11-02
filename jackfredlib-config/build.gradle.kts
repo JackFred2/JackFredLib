30c184146835da5e3c499fa12ebae497d7609910
Jack Pearce <jack@jackf.red>
2023-11-02 15:16:13 +0000

    [internal] setup config tests

--- jackfredlib-config/build.gradle.kts
+++ jackfredlib-config/build.gradle.kts
@@ -4,6 +4,21 @@ base {
 
 dependencies {
     include(implementation("blue.endless:jankson:${properties["jankson_version"]}")!!)
+
+    testImplementation("net.fabricmc:fabric-loader-junit:${properties["loader_version"]}")
+}
+
+sourceSets {
+    test {
+        compileClasspath += client.get().compileClasspath
+        runtimeClasspath += client.get().runtimeClasspath
+        compileClasspath += client.get().output
+        runtimeClasspath += client.get().output
+    }
+}
+
+tasks.withType<Test> {
+    this.useJUnitPlatform()
 }
 
 @Suppress("UNCHECKED_CAST")
--- jackfredlib-config/src/main/java/red/jackf/jackfredlib/api/config/migration/builtin/Identity.java
+++ jackfredlib-config/src/main/java/red/jackf/jackfredlib/api/config/migration/builtin/Identity.java
@@ -9,6 +9,10 @@ import red.jackf.jackfredlib.api.config.migration.MigrationResult;
 
 import java.util.function.Supplier;
 
+/**
+ * A test 'migration' that does nothing to the JSON.
+ * @param <T> Type of the config which the JSON will end up as.
+ */
 public class Identity<T extends Config<T>> implements Migration<T> {
 
     @Override
@@ -18,6 +22,6 @@ public class Identity<T extends Config<T>> implements Migration<T> {
             Jankson jankson,
             Version oldVersion,
             Version newVersion) {
-        return MigrationResult.SUCCESS("passthrough");
+        return MigrationResult.SUCCESS;
     }
 }
