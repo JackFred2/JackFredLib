46472e64461095342a6a65d33dab5d89eaadaa28
Jack Pearce <jack@jackf.red>
2023-12-01 22:08:15 +0000

    [fix] csd copy issue at origin; flag mixin extras as required; bump version

--- jackfredlib-extracommandsourcedata/src/main/java/red/jackf/jackfredlib/impl/extracommandsourcedata/CommandSourceStackUtils.java
+++ jackfredlib-extracommandsourcedata/src/main/java/red/jackf/jackfredlib/impl/extracommandsourcedata/CommandSourceStackUtils.java
@@ -6,6 +6,6 @@ public class CommandSourceStackUtils {
     public static CommandSourceStack copy(CommandSourceStack original) {
         // cheap hack to create a new instance with the same values
         var oldPos = original.getPosition();
-        return original.withPosition(oldPos.reverse()).withPosition(oldPos);
+        return original.withPosition(oldPos.add(0, 1, 0)).withPosition(oldPos);
     }
 }
