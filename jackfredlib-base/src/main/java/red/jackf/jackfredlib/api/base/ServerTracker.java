289be50bce239a6db5c295575e3c742a3624d100
Jack Pearce <jack@jackf.red>
2024-01-08 19:25:23 +0000

    [docs] add server tracker docs

--- jackfredlib-base/src/main/java/red/jackf/jackfredlib/api/base/ServerTracker.java
+++ jackfredlib-base/src/main/java/red/jackf/jackfredlib/api/base/ServerTracker.java
@@ -1,11 +1,23 @@
 package red.jackf.jackfredlib.api.base;
 
 import net.minecraft.server.MinecraftServer;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import red.jackf.jackfredlib.impl.base.ServerTrackerImpl;
 
+/**
+ * Static access for the current Minecraft Server instance.
+ */
+@ApiStatus.AvailableSince("1.3.0")
 public interface ServerTracker {
+    /**
+     * Obtain the current server tracker instance.
+     */
     ServerTracker INSTANCE = new ServerTrackerImpl();
 
+    /**
+     * Get the current MinecraftServer instance, if available.
+     * @return The currently running MinecraftServer instance, or {@code null} if not available.
+     */
     @Nullable MinecraftServer getServer();
 }
