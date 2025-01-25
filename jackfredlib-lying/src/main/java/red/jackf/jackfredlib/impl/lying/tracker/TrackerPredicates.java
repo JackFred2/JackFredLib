c74bc127da742f142a1eef4a4d3c449891256b5c
Jack Pearce <jack@jackf.red>
2025-01-25 17:58:15 +0000

    [feat] entity and custom positional predicates for trackers (close #5)

--- jackfredlib-lying/src/main/java/red/jackf/jackfredlib/impl/lying/tracker/TrackerPredicates.java
+++ jackfredlib-lying/src/main/java/red/jackf/jackfredlib/impl/lying/tracker/TrackerPredicates.java
@@ -1,7 +1,9 @@
 package red.jackf.jackfredlib.impl.lying.tracker;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Predicate;
 
@@ -14,5 +16,11 @@ public interface TrackerPredicates {
 
     static Predicate<Vec3> forBounds(AABB bounds) {
         return bounds::contains;
+    }
+
+    static Predicate<Vec3> forEntity(@NotNull Entity focus, double radius) {
+        final double radiusSqr = radius * radius;
+
+        return pos -> pos.distanceToSqr(focus.position()) < radiusSqr;
     }
 }
